4bf47e2a382292d50d27b5686dc2cc3665b14954
gfrdindia <gfrdindia@example.com>
2026-02-27 21:40:09 +0530

    first dry run version 1.0.0

--- helper-auth-service/src/main/java/com/helper/auth/config/DataInitializer.java
+++ helper-auth-service/src/main/java/com/helper/auth/config/DataInitializer.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("dev")
+@Profile({"dev", "local"})
 @RequiredArgsConstructor
 @Slf4j
 public class DataInitializer implements CommandLineRunner {
